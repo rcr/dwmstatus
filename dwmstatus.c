a4749018015e47157e01703fe418868bebcff5d0
Richard Robbins <mail@rcr.io>
2018-02-22 23:11:26 -0800

    Add sleep time as parameter

--- dwmstatus.c
+++ dwmstatus.c
@@ -5,7 +5,6 @@
 #include <X11/Xlib.h>
 
 #define STRING_MAX 256
-#define SLEEP_TIME  60
 
 static const char *draw_bat(void);
 static const char *draw_time(void);
@@ -14,9 +13,7 @@ static const char *draw_vol(void);
 static const char*
 draw_bat(void)
 {
-	static char str[STRING_MAX];
-
-	char s = 0, *err = NULL;
+	static char *err = NULL, str[STRING_MAX];
 
 	int c0 = 0,
 	    c1 = 0;
@@ -26,6 +23,8 @@ draw_bat(void)
 	     *fd_2 = fopen("/sys/class/power_supply/BAT0/charge_full_design", "r");
 
 	if (fd_0 && fd_1 && fd_2) {
+
+		char s = 0;
 
 		switch (fgetc(fd_0)) {
 			case 'C': s = '+'; break;
@@ -45,13 +44,13 @@ draw_bat(void)
 		if (!err && (c0 <= 0 || c1 <= 0))
 			err = "err: invalid";
 
-		if (!err && 0 > snprintf(str, sizeof(str), "%c %d%%", s, (int)(c0 * 100.0 / c1)))
+		if (!err && 0 > snprintf(str, sizeof(str), "%c%d%%", s, (int)(c0 * 100.0 / c1)))
 			err = "err: snprintf";
 	}
 
-	if (fd_0) fclose(fd_0) else err = "err: fd_0";
-	if (fd_1) fclose(fd_1) else err = "err: fd_1";
-	if (fd_2) fclose(fd_2) else err = "err: fd_2";
+	if (fd_0) fclose(fd_0); else err = "err: fd_0";
+	if (fd_1) fclose(fd_1); else err = "err: fd_1";
+	if (fd_2) fclose(fd_2); else err = "err: fd_2";
 
 	return err ? err : str;
 }
@@ -59,13 +58,14 @@ draw_bat(void)
 static const char*
 draw_time(void)
 {
-	static char str[STRING_MAX];
+	static char *err = NULL, str[STRING_MAX];
 
 	time_t now = time(0);
 
-	strftime(str, STRING_MAX, "%a %e  %R", localtime(&now));
+	if (0 == strftime(str, sizeof(str), "%a %e  %R", localtime(&now)))
+		err = "err: snprintf";
 
-	return str;
+	return err ? err : str;
 }
 
 static const char*
@@ -77,14 +77,19 @@ draw_vol(void)
 }
 
 int
-main(void)
+main(int argc, char **argv)
 {
+	unsigned int seconds = 0;
+
+	if (argc > 1)
+		seconds = (unsigned int)(strtol(argv[1], NULL, 0));
+
 	Display *display;
 
 	if (!(display = XOpenDisplay(NULL)))
 		exit(EXIT_FAILURE);
 
-	for (;;) {
+	do {
 		char status[STRING_MAX];
 
 		snprintf(status, sizeof(status), " %s  ~  %s  ~  %s ",
@@ -93,8 +98,9 @@ main(void)
 		XStoreName(display, DefaultRootWindow(display), status);
 		XSync(display, 0);
 
-		sleep(SLEEP_TIME);
-	}
+		sleep(seconds);
+
+	} while (seconds);
 
 	XCloseDisplay(display);
 
